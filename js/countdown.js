13e6cb050c0f33fdb532e5e4c386afafa1c62f54
WoutDemeyere <wout.demeyere@student.howest.be>
2020-12-29 14:04:14 +0100

    fixed spacing+sizing

--- js/countdown.js
+++ js/countdown.js
@@ -9,8 +9,8 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-    document.querySelector(".js-timer").innerHTML ="Nog " + days + "d " + hours + "h " 
-    + minutes + "m " + seconds + "s " + "tot de stream start";
+    document.querySelector(".js-timer").innerHTML ="Nog " + days + " Dagen " + hours + " Uur " 
+    + minutes + " Min " + seconds + " Sec " + "tot de stream start";
   
     if (distance < 0) {
       clearInterval(x);
